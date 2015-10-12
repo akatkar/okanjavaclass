701c78e4ea5b635f88b9ef28e66643d2229a83f0
Ali Katkar <alikatkar@gmail.com>
2015-10-12 20:55:27 +0300

    Homework01 hazır

---
+++ JavaLessons/src/homeworks/homework01/Circle.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homeworks.homework01;
+
+/**
+ *
+ * @author alikatkar
+ */
+public class Circle {
+    
+    public static void main(String[] args){
+        
+        // double tipinde PI değişkeni tanımlayın. ilk değeri 3.14159
+        // double tipinde radius (yarı çap) isimli bir değişken tanımlayın
+        // ilk değeri 21 olsun.
+        // Bu dairenin çevresini ve alanını hesaplayan kodu yazın.
+        
+        
+        
+    }        
+}
